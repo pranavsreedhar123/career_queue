a4603a145489d5817c7423a6eb503bf905cc0d0e
Amirita <amanickandan6@gatech.edu>
2024-09-29 03:44:48 -0400

    amis final changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,31 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
-import * as React from 'react'
-
-
-// 1. import `ChakraProvider` component
-import { ChakraProvider } from '@chakra-ui/react'
+import * as React from 'react';
+import { ChakraProvider, Box } from '@chakra-ui/react';
 
 function App() {
   return (
+    <ChakraProvider>
+      {/* Gold background that stays behind the content */}
+      <Box
+        position="fixed"
+        top="0"
+        left="0"
+        width="100vw"
+        height="100vh"
+        backgroundColor="gold"
+        zIndex="-1"  /* Ensure it is behind the header */
+      />
 
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-
+      <div className="App">
+        <header className="App-header">
+          {/* Existing content of your app */}
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    </ChakraProvider>
   );
 }
 
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,23 +1,23 @@
-import React from 'react'
-import './Navbar.css'
+import React from 'react';
+import './Navbar.css';
 import { useNavigate } from 'react-router-dom';
+import logo from '../logo.png'; // Adjust the path based on your project structure
 
 const Navbar = () => {
     const navigate = useNavigate();
 
     return (
         <div className='navbar'>
-            <img src="" alt="" className='logo' />
+            {/* Add the logo */}
+            <img src={logo} alt="Logo" className='logo' />
             <ul>
-                <li onClick={() => navigate('/')}> Profile</li>
+                <li onClick={() => navigate('/')}>Profile</li>
                 <li onClick={() => navigate('/map')}>Map</li>
                 <li onClick={() => navigate('/queue')}>Queue</li>
             </ul>
             <img src="" alt="" className='toggle-icon' />
         </div>
-    )
-
-}
+    );
+};
 
 export default Navbar;
-
